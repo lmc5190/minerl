b32c1d66e2f86d37c2b98ec6108e913d16e63dad
Landon Chambers <landon_chambers@SAAS.LOCAL@gpu2.saas.local>
2019-06-24 23:46:45 -0500

    building out cloning. defined cnn for image state only, no compass or dirt state yet.

--- cloning/navigatedense.py
+++ cloning/navigatedense.py
@@ -5,6 +5,34 @@ import itertools
 import numpy as np
 import torch
 
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 4, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(4, 8, 4)
+        self.conv3 = nn.Conv2d(8, 16, 3)
+        self.fc1 = nn.Linear(16 * 6 * 6, 140)
+        self.fc2 = nn.Linear(140, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x))) # 64 -> 62 -> 31 
+        x = self.pool(F.relu(self.conv2(x))) # 31 -> 28 -> 14
+        x = self.pool(f.relu(self.conv3(x))) # 14 -> 12 -> 6
+        x = x.view(-1, 16 * 6 * 6) 
+        x = F.relu(self.fc1(x)) # 16*6*6 -> 140
+        x = F.relu(self.fc2(x)) # 140 -> 84
+        x = self.fc3(x)         # 84 -> 10
+        return x
+
+
+net = Net()
+
 def normalize_tensor(x,xmin,xmax,a,b):
     #transform elements of tensor x in approximate range [xmin,xmax] to [a,b
     A = (b-a)/(xmax-xmin)
@@ -62,6 +90,9 @@ for obs, rew, done, act in data.seq_iter(num_epochs=1, max_sequence_len=batch_si
         dirt = normalize_tensor(dirt,0,64.0*36,-1.0,1.0)
         deltapitch =  normalize_tensor(deltapitch,-100.0,100.0,0,1.0)
         deltayaw =  normalize_tensor(deltayaw,-180.0,180.0,0,1.0)
+
+    #reshape image tensor
+    img=img.view(true_batch_size,3,64,64)
 
     #shape action tensor (true_batch_size, 10)
     action = torch.cat((attack,back,forward,jump,right,\
